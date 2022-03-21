f2507cff03f99d2d671bf085f012c9c9213058db
Anna Svetlova <svetloovaanna@gmail.com>
2022-03-21 21:54:57 +0200

    1

--- task1/script.js
+++ task1/script.js
@@ -4,8 +4,10 @@ function onSearch () {
     fetch ('https://api.giphy.com/v1/gifs/search?api_key=4lx65c3QTkwqwsVrRHOshsHUC698iiES&q=' + getGiphy + '&limit=5&offset=0&rating=g&lang=ru')
         .then(response => response.json())
         .then(giphy => {
-            document.getElementById("place").src = giphy.url;
-            document.getElementById("qw").innerText = giphy.title;
+            console.log(giphy);
+            document.getElementById("place").src = giphy.data.url;
+            // document.getElementById("qw").innerText = data.id;
         })
         .catch(error => console.log(error));
 }
+
