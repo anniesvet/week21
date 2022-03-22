3f68513401c10db1c4212714709ef7593250d2f2
Anna Svetlova <svetloovaanna@gmail.com>
2022-03-22 14:55:28 +0200

    add index

--- task1/script.js
+++ task1/script.js
@@ -3,10 +3,8 @@ function onSearch () {
 
     fetch ('https://api.giphy.com/v1/gifs/search?api_key=4lx65c3QTkwqwsVrRHOshsHUC698iiES&q=' + getGiphy + '&limit=5&offset=0&rating=g&lang=ru')
         .then(response => response.json())
-        .then(giphy => {
-            console.log(giphy);
-            // document.getElementById("place").src = giphy.data.url;
-            document.getElementById("qw").innerText = JSON.stringify(giphy.data);
+        .then(data => {
+            document.getElementById("place").src = data[0].url;
         })
         .catch(error => console.log(error));
 }
