8a1ff3b1ecd920e78d0d2e5ff758269e4c9d09c7
Anna Svetlova <svetloovaanna@gmail.com>
2022-03-23 19:30:32 +0200

    added index

--- task1/script.js
+++ task1/script.js
@@ -3,9 +3,23 @@ function onSearch () {
 
     fetch ('https://api.giphy.com/v1/gifs/search?api_key=4lx65c3QTkwqwsVrRHOshsHUC698iiES&q=' + getGiphy + '&limit=5&offset=0&rating=g&lang=ru')
         .then(response => response.json())
-        .then(data => {
-            document.getElementById("place").src = data[0].url;
+        .then(res => {
+            document.getElementById('name1').innerHTML = res.data[0].title
+            document.getElementById('place1').src = res.data[0].images.original.url
+
+            document.getElementById('name2').innerHTML = res.data[1].title
+            document.getElementById('place2').src = res.data[1].images.original.url
+
+            document.getElementById('name3').innerHTML = res.data[2].title
+            document.getElementById('place3').src = res.data[2].images.original.url
+
+            document.getElementById('name4').innerHTML = res.data[3].title
+            document.getElementById('place4').src = res.data[3].images.original.url
+
+            document.getElementById('name5').innerHTML = res.data[4].title
+            document.getElementById('place5').src = res.data[4].images.original.url
         })
         .catch(error => console.log(error));
 }
+
 
